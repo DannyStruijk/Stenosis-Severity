83fe70daa38eb763530a920b86b71c8d4b2c83a4
Danny Struijk <danny.struijk@catharinaziekenhuis.nl>
2025-02-27 13:51:20 +0100

    Edited Code

--- b-spline_fitting/leaflet_surface_NURBS.py
+++ b-spline_fitting/leaflet_surface_NURBS.py
@@ -1,25 +1,40 @@
-from geomdl import BSpline
+# Script to use the boundaries created in leaflet_boundaries.py to reconstruct
+# the leaflet based on a NURBS surface. Consequently, export the file and view 
+# surface to a vtk file to view it in paraview.
 
+from geomdl import fitting, BSpline
+import numpy as np
 
-# Savebtk
+# Define control points based on user's input
+point_1 = [0, 0, 1]  # Commissure 1
+point_2 = [2, 0, 1]  # Commissure 2
+hinge_point = [1, 0, 0]  # Hinge point
+leaflet_tip = [(point_1[0] + point_2[0]) / 2, 1, (point_1[2] + hinge_point[2]) / 3]
+arch_control_1 = [(leaflet_tip[0] + point_1[0]) / 2, (leaflet_tip[1] + hinge_point[1]) / 2, (leaflet_tip[2] + hinge_point[2]) / 1.5]
+arch_control_2 = [(leaflet_tip[0] + point_2[0]) / 2, (leaflet_tip[1] + hinge_point[1]) / 2, (leaflet_tip[2] + hinge_point[2]) / 1.5]
 
-# Create the curve instance
-crv = BSpline.Curve()
+# Create boundary curves
+curve_1 = [point_1, arch_control_1, leaflet_tip]
+curve_2 = [leaflet_tip, arch_control_2, point_2]
+curve_3 = [point_1, hinge_point, point_2]  # curve from point_1 to point_2 via hinge_point
 
-# Set degree
-crv.degree = 2
+# Define the control grid (3x3 control points)
+control_points = np.array([
+    [point_1, arch_control_1, leaflet_tip],
+    [hinge_point, hinge_point, hinge_point],
+    [point_2, arch_control_2, leaflet_tip]
+])
 
-# Set control points
-crv.ctrlpts = [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
+# Set degree to 3 (cubic)
+degree_u = 3
+degree_v = 3
 
-# Set knot vector
-crv.knotvector = [0, 0, 0, 1, 1, 1]
+# Set the size of the surface in u and v directions
+size_u = 2
+size_v = 2
+ 
+# Fit NURBS surface through the control points
+surf = fitting.interpolate_surface(control_points, size_u, size_v, degree_u, degree_v)
 
-# Import Matplotlib visualization module
-from geomdl.visualization import VisMPL
-
-# Set the visualization component of the curve
-crv.vis = VisMPL.VisCurve3D()
-
-# Plot the curve
-crv.render()
+# Set evaluation delta for resolution
+surf.delta = 0.05
