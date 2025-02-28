675abcc30b61ce6e3646df7dd7a48f573c9789b5
Danny Struijk <danny.struijk@catharinaziekenhuis.nl>
2025-02-28 09:29:57 +0100

    Edited Files

--- b-spline_fitting/leaflet_surface_NURBS.py
+++ b-spline_fitting/leaflet_surface_NURBS.py
@@ -2,8 +2,10 @@
 # the leaflet based on a NURBS surface. Consequently, export the file and view 
 # surface to a vtk file to view it in paraview.
 
-from geomdl import fitting, BSpline
+from geomdl import BSpline
 import numpy as np
+import pyvista as pv
+from functions import export_vtk
 
 # Define control points based on user's input
 point_1 = [0, 0, 1]  # Commissure 1
@@ -14,25 +16,25 @@ arch_control_1 = [(leaflet_tip[0] + point_1[0]) / 2, (leaflet_tip[1] + hinge_poi
 arch_control_2 = [(leaflet_tip[0] + point_2[0]) / 2, (leaflet_tip[1] + hinge_point[1]) / 2, (leaflet_tip[2] + hinge_point[2]) / 1.5]
 
 # Define the control grid (3x3 control points)
-control_points = ([
+control_points = [
     [point_1, arch_control_1, leaflet_tip],
     [hinge_point, hinge_point, hinge_point],
     [point_2, arch_control_2, leaflet_tip]
-])
+]
 
-# Define surface
+# Define the NURBS surface
 surf = BSpline.Surface()
 surf.degree_u = 2
 surf.degree_v = 2
-surf.ctrlpts2d = control_points # Set control points
+surf.ctrlpts2d = control_points  # Set control points
 
 # Define knot vectors
 surf.knotvector_u = [0, 0, 0, 1, 1, 1]
 surf.knotvector_v = [0, 0, 0, 1, 1, 1]
 
 # Set resolution
-surf.delta = 0.05 # Resolution
+surf.delta = 0.05  # Resolution
 
-# Fit NURBS surface through the control points
 surf.evaluate()
 
+export_vtk(surf, "H:/DATA/Afstuderen/2. Code/Stenosis-Severity/b-spline_fitting/leaflet_surface.vtk")
