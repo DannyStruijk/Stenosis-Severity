8873fac83fe8a52f9e08246bb6677b832d592280
Danny Struijk <danny.struijk@catharinaziekenhuis.nl>
2025-02-28 12:07:32 +0100

    Edited

--- b-spline_fitting/leaflet_surface_NURBS.py
+++ b-spline_fitting/leaflet_surface_NURBS.py
@@ -5,6 +5,7 @@
 import numpy as np
 import functions
 import os 
+from geomdl.visualization import VisMPL
 
 # Change working directory in order to import functions from other file
 os.chdir("H:/DATA/Afstuderen/2. Code/Stenosis-Severity/b-spline_fitting")
@@ -19,15 +20,14 @@ leaflet_tip = [(commissure_1[0] + commissure_2[0]) / 2, 1, (commissure_1[2] + hi
 control_points = functions.calc_surface_ctrlpts(commissure_1, commissure_2, leaflet_tip, hinge_point)
 
 # Define the knot vectors for the surface
-knotvector_u = [0, 0, 0, 0.9, 0.9, 1]
-#knotvector_v = [0, 0, 0, 0.9, 0.9, 1]
+knotvector_u = [0, 0.01, 0.4, 0.6, 0.8, 1]
+knotvector_v = [0, 0.01, 0.4, 0.6, 0.8, 1]
 
 #knotvector_u = [0, 0.25, 0.5, 0.75, 0.9, 1]
-knotvector_v = [0, 0.25, 0.5, 0.75, 0.9, 1]
+#knotvector_v = [0, 0.25, 0.5, 0.75, 0.9, 1]
 
 # Reconstruct the surface using the defined control points, parameters are currently hard-coded
 surf = functions.reconstruct_surface(control_points, knotvector_u = knotvector_u, knotvector_v = knotvector_v)
 
 # Save the surface as a VTK file
 functions.export_vtk(surf, "H:/DATA/Afstuderen/2. Code/Stenosis-Severity/reconstructions/leaflet_surface.vtk")
-
